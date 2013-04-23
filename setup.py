3c66ac4e840d3fabcd113807b9c758958dc4585a
Jim Fulton <jim@zope.com>
2013-04-23 15:16:14 -0400

    prep for release.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.sqs', '0'
+name, version = 'zc.sqs', '0.1.0'
 
 install_requires = ['setuptools', 'boto', 'ZConfig']
 extras_require = dict(test=['zope.testing', 'manuel', 'mock', 'zc.thread'])
